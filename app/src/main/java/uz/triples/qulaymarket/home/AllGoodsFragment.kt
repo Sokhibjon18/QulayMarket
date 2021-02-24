08664a369530043aa0f28241f95235d9429189af
Xudoyshukur <Khudoyshukur.Juraev.001@mail.ru>
2021-02-25 00:24:35 +0500

    worked on calendar(ChangeBirthdayFragment)

--- app/src/main/java/uz/triples/qulaymarket/home/AllGoodsFragment.kt
+++ app/src/main/java/uz/triples/qulaymarket/home/AllGoodsFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
--- app/src/main/java/uz/triples/qulaymarket/profile/ChangeBirthdayFragment.kt
+++ app/src/main/java/uz/triples/qulaymarket/profile/ChangeBirthdayFragment.kt
@@ -9,8 +9,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_change_birthday.view.*
+import kotlinx.android.synthetic.main.fragment_change_birthday.view.save_button
+import kotlinx.android.synthetic.main.fragment_change_name.view.*
 import uz.triples.qulaymarket.R
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ChangeBirthdayFragment : Fragment() {
 
@@ -31,13 +37,48 @@ class ChangeBirthdayFragment : Fragment() {
 
             if(event.action == MotionEvent.ACTION_UP) {
                 if(event.rawX >= ((view.birthday.editText as EditText).right - (view.birthday.editText as EditText).compoundDrawables[drawableRight].bounds.width())) {
-                    Toast.makeText(requireContext(), "Calendar icon click!", Toast.LENGTH_SHORT).show()
-                    
+                    if(view.birthday_picker.visibility == View.VISIBLE){
+                        view.birthday_picker.visibility = View.GONE
+                    } else{
+                        view.birthday_picker.visibility = View.VISIBLE
+                        val date = try {
+                            SimpleDateFormat("dd-MM-yyyy", Locale.ROOT).parse((view.birthday.editText as EditText).text.toString())
+                        } catch (e: Exception){
+                            Date()
+                        }
+                        view.birthday_picker.clearSelection()
+                        view.birthday_picker.setDateSelected(date,true)
+                        view.birthday_picker.setCurrentDate(date)
+                    }
                 }
             }
-            true
+            false
+        }
+
+        view.birthday_picker.setOnDateChangedListener { widget, date, selected ->
+            val dateFormat = SimpleDateFormat("dd-MM-yyyy", Locale.ROOT)
+
+            val dateText = dateFormat.format(date.date)
+
+            view.birthday.editText?.setText(dateText)
+            view.birthday_picker.visibility = View.GONE
+        }
+
+        view.save_button.setOnClickListener {
+            val text = view.birthday.editText?.text.toString()
+            if(text.isBlank()){
+                Toast.makeText(requireContext(), "Tug'ilgan kuningizni kiriting", Toast.LENGTH_SHORT).show()
+            } else{
+                Toast.makeText(requireContext(), "Birthday is inserted to the database", Toast.LENGTH_SHORT).show()
+                findNavController().navigate(R.id.action_changeBirthdayFragment_to_myProfileDetails)
+            }
         }
 
         return view
+    }
+
+    private fun getDateFormat(): String{
+
+        return ""
     }
 }
